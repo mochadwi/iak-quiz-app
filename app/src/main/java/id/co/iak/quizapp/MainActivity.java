f9725d7a43289f033b1e265d8f48d3b55977db36
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-26 21:54:49 +0700

    [Add] Get started button to go to first question

--- app/src/main/java/id/co/iak/quizapp/MainActivity.java
+++ app/src/main/java/id/co/iak/quizapp/MainActivity.java
@@ -1,13 +1,32 @@
 package id.co.iak.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Views
+    @BindView(R.id.btn_continue)
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_question);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent());
+                Toast.makeText(MainActivity.this, "Click to continue!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
