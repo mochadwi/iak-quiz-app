ad0113a4fdf5291de11740fcf7cc3297bc0f083e
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-28 11:16:19 +0700

    [Add] Question 1 - 5, fix layouting

---
+++ app/src/main/java/id/co/iak/quizapp/question/Question04Activity.java
@@ -0,0 +1,15 @@
+package id.co.iak.quizapp.question;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.co.iak.quizapp.R;
+
+public class Question04Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_04_question);
+    }
+}
