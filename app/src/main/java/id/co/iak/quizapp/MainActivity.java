0b0756aa9de49a4f68337faba7be609e596abc77
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-05-03 11:15:11 +0700

    [Update]  to Android Studio 2.3

--- app/src/main/java/id/co/iak/quizapp/MainActivity.java
+++ app/src/main/java/id/co/iak/quizapp/MainActivity.java
@@ -9,49 +9,72 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import id.co.iak.quizapp.model.QuestionModel;
 import id.co.iak.quizapp.model.UserModel;
 import id.co.iak.quizapp.question.Question01Activity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Views
-    @BindView(R.id.btn_continue)
-    Button btnContinue;
-    @BindView(R.id.edt_name)
-    TextView edtName;
-    @BindView(R.id.edt_email)
-    TextView edtEmail;
-    @BindView(R.id.cb_eula)
-    CheckBox cbEula;
+	// Views
+	@BindView(R.id.btn_continue)
+	Button btnContinue;
+	@BindView(R.id.edt_name)
+	TextView edtName;
+	@BindView(R.id.edt_email)
+	TextView edtEmail;
+	@BindView(R.id.cb_eula)
+	CheckBox cbEula;
 
-    // Data
-    private String name, email;
+	// Data
+	private String name, email;
+	private List<QuestionModel> questions = new ArrayList<>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
 
-        btnContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cbEula.isChecked()) {
-                    name = edtName.getText().toString();
-                    email = edtEmail.getText().toString();
-                    UserModel user = new UserModel(name, email, 0);
-                    Toast.makeText(MainActivity.this, "Ready!", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(MainActivity.this, Question01Activity.class);
-                    i.putExtra("biodata", user.toString());
-                    startActivity(i);
-                    finish();
-                } else {
-                    Toast.makeText(MainActivity.this, "Please ensure your submission!",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
+		btnContinue.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// Prepare Question Model
+				prepareQuestion();
+				name = edtName.getText().toString();
+				email = edtEmail.getText().toString();
+				if (cbEula.isChecked() && !name.isEmpty() && !email.isEmpty()) {
+					UserModel user = new UserModel(name, email, 0);
+					Toast.makeText(MainActivity.this, "Ready!", Toast.LENGTH_SHORT).show();
+					Intent i = new Intent(MainActivity.this, Question01Activity.class);
+					i.putExtra("biodata", user.toString());
+					startActivity(i);
+					finish();
+				} else {
+					Toast.makeText(MainActivity.this, "Please ensure your submission!",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+
+	private void prepareQuestion() {
+		final List<String> answers = new ArrayList<>();
+		answers.add("\"Final Fantasy XIII\""); // Right Answer
+		answers.add("\"Final Fantasy XII\"");
+		answers.add("\"Final Fantasy VII: Crisis Core\"");
+		answers.add("\"Final Fantasy\"");
+		final QuestionModel question = new QuestionModel(
+				"Almost every installment of \"Final Fantasy\"" +
+						"began with the melody called \"Prelude\", " +
+						"and we were always rewarded with \"Victory Fanfare\" after a battle. \n\n" +
+						"Which title is the only game that does not include those staple tunes?",
+				"", // Explanation
+				"\"Final Fantasy XIII\"", answers, 20);
+		questions.add(question);
+	}
 }
